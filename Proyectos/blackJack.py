b89cb7c63dab8245f53607110b906e5288fa4be4
Luka <lukasanguinetti@gmail.com>
2026-01-21 16:53:57 -0300

    reconstruccion del blackjack, SumaValores

--- Proyectos/blackJack.py
+++ Proyectos/blackJack.py
@@ -5,54 +5,49 @@ mazo = {"A♠": 11,"2♠": 2,"3♠": 3,"4♠": 4,"5♠": 5,"6♠": 6,"7♠": 7,"
         "A♦": 11,"2♦": 2,"3♦": 3,"4♦": 4,"5♦": 5,"6♦": 6,"7♦": 7,"8♦": 8,"9♦": 9,"10♦": 10,"J♦": 10,"Q♦": 10,"K♦": 10,
         "A♣": 11,"2♣": 2,"3♣": 3,"4♣": 4,"5♣": 5,"6♣": 6,"7♣": 7,"8♣": 8,"9♣": 9,"10♣": 10,"J♣": 10,"Q♣": 10,"K♣": 10}
 def Jugar():
-    print("Vamos jugar Black Jack !!!")
     mazoUsado = mazo
-    cartaSacada, segundaCarta = random.choice(list(mazoUsado.keys())),random.choice(list(mazoUsado.keys()))
-    mano = [cartaSacada,segundaCarta]
-    sumarCartasJugador = mazoUsado.pop(cartaSacada) + mazoUsado.pop(segundaCarta)
+    cartasJugador = []
+    cartasCrupier = []
 
-    print(f"Tu mano es: {cartaSacada} y {segundaCarta} || esto suma: {sumarCartasJugador} \n")
+    def RepatirCartaJugador(jugador):
+        if not jugador:
+            carta1 = random.choice(list(mazoUsado.keys()))
+            carta2 = random.choice(list(mazoUsado.keys()))
+            jugador.extend([carta1,carta2])
+            del mazoUsado[carta1]
+            del mazoUsado[carta2]
+        else:
+            carta = random.choice(list(mazoUsado.keys()))
+            jugador.append(carta)
+            del mazoUsado[carta1]
+        
+        return jugador
 
-    sigue = input("queres otra carta? s/n: ")
-    while sigue == "s":
-        cartaSacada = random.choice(list(mazoUsado.keys()))
-        mano.append(cartaSacada)
-        sumarCartasJugador += mazoUsado.pop(cartaSacada)
-        print(f"sacaste un {cartaSacada} || tu mano es {mano} || esto suma: {sumarCartasJugador}\n")
-        if sumarCartasJugador > 21:
-            print("PERDISTE - sumaste mas de 21")
-            return 
-        elif sumarCartasJugador == 21:
-            print("GANASTE!!!!")
-        sigue = input("queres otra carta? s/n: ")
-    print(f"te quedaste con {sumarCartasJugador}\n")
+    def SumaTotal(cartas):
+        listaValores = [mazo[carta] for carta in cartas]
+        total = 0
+        for valor in listaValores:
+            if 11 in listaValores and (total + valor > 21):
+                esAz = lambda carta: carta == 11
+                aces = [carta for carta in listaValores if esAz(carta)]
+                resto = [carta for carta in listaValores if not esAz(carta)]
 
-    print("Ahora juega la Banca")
-    cartaSacada, segundaCarta = random.choice(list(mazoUsado.keys())),random.choice(list(mazoUsado.keys()))
-    mano = [cartaSacada,segundaCarta]
-    sumarCartas = mazoUsado.pop(cartaSacada) + mazoUsado.pop(segundaCarta)
-    print(f"La banca tiene: {cartaSacada} y {segundaCarta} || esto suma: {sumarCartas} \n")
-    while sumarCartas < 17:
-        cartaSacada = random.choice(list(mazoUsado.keys()))
-        mano.append(cartaSacada)
-        sumarCartas += mazoUsado.pop(cartaSacada)
-        print(f"saco un {cartaSacada} || la mano de la banca es {mano} || esto suma: {sumarCartas}\n")
-    if sumarCartas > 21:
-        print("LA BANCA PIERDE")
-        return
-    elif sumarCartas == 21:
-        print("LA BANCA GANA")
-        return
-    print(f"la banca se queda con {sumarCartas}\n")
-    print(sumarCartasJugador)
-    print(sumarCartas)
-    if sumarCartasJugador > sumarCartas:  
-        print("GANASTE!!!!")
-        return
-    else:
-        print("PERDISTE")
-        return
+                subTotal = sum(resto)
 
-    mazoUsado = mazo
-    
-Jugar()
+                for i in range(len(aces)):
+                    if subTotal + 11 <= 21:
+                        subTotal += 11
+                    else:
+                        subTotal += 1
+                    
+            else:
+                total += valor
+
+            def contieneAz(cartas):
+                for carta in cartas:
+                    if carta[0] == "A":
+                        return True
+                return False
+        
+
+        return total
