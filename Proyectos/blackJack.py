0bd7975488a2a11b12b7211db3977f52e231439b
Luka <lukasanguinetti@gmail.com>
2026-01-23 15:53:22 -0300

    funciones de perder y BlackJack, tambien edito el formato de salida de las cartas

--- Proyectos/blackJack.py
+++ Proyectos/blackJack.py
@@ -14,6 +14,7 @@ def RepatirCartaJugador(jugador):
         jugador.extend([carta1,carta2])
         del mazoUsado[carta1]
         del mazoUsado[carta2]
+          
     else:
         carta = random.choice(list(mazoUsado.keys()))
         jugador.append(carta)
@@ -29,44 +30,64 @@ def SumaTotal(cartas, mazo):
 
     total = 0
     for valor in listaValores:
-        if 11 in set(listaValores) and (total + valor > 21):
+        if (11 in set(listaValores)) and (total + valor > 21):
             esAz = lambda carta: carta == 11
             aces = [carta for carta in listaValores if esAz(carta)]
             resto = [carta for carta in listaValores if not esAz(carta)]
-
             subTotal = sum(resto)
 
+            if len(aces) == 2:
+                total += 2
             for aces in range(len(aces)):
                 if subTotal + 11 <= 21:
                     subTotal += 11
                 else:
                     subTotal += 1
+                    total = subTotal
                 
         else:
             total += valor
+        
 
         def contieneAz(cartas):
             for carta in cartas:
                 if carta[0] == "A":
                     return True
             return False
-
     return total  
 
 def PedirCarta(jugador):
     quiereOtra = input("Queres otra carta?: s/n ")
     if quiereOtra == "s":
         RepatirCartaJugador(jugador)
+        print("Tus cartas son: ",jugador, " | esto suma: ",SumaTotal(jugador, mazo))
+        
+        if not Perdio(jugador):
+            PedirCarta(jugador)
+        else: 
+            print("--- Perdiste --- :(")
+        return 
         
     else: 
-        pass
+        print("Te quedate con: ",jugador, " | esto suma: ",SumaTotal(jugador, mazo))
+
+def Perdio(jugador):
+    if SumaTotal(jugador,mazo) > 21:
+        return True
+def BlackJack(jugador):
+    if SumaTotal(jugador, mazo) == 21:
+        return True
 
 
-jugador = []
-RepatirCartaJugador(jugador)
-print(jugador)
-print(SumaTotal(jugador, mazo))
+##### MAIN
 
-PedirCarta(jugador)
-print(jugador)
-print(SumaTotal(jugador, mazo))
+def Jugar(jugador):
+    print("--------- Black Jack --------- ")
+    RepatirCartaJugador(jugador)
+    print("Tus cartas son: -"," ".join(jugador), "- | esto suma: ",SumaTotal(jugador, mazo))
+    if BlackJack(jugador):
+        print("¡¡¡ GANASTE !!!")
+        return
+    PedirCarta(jugador)
+
+Jugar([])
